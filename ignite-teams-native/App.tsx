c60a93284b44bd8dcacb25fe14da41b4f01b58a8
LucasWermann <lucasvw.eng@gmail.com>
2023-06-07 23:19:01 -0300

    Add: Fontes c/ expo-googles-fonts e Componente Loading

--- ignite-teams-native/App.tsx
+++ ignite-teams-native/App.tsx
@@ -1,13 +1,22 @@
-import { ThemeProvider } from 'styled-components';
 import { StatusBar } from 'react-native'
-import { Groups } from '@screens/Groups'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 import theme from './src/theme'
+import { ThemeProvider } from 'styled-components';
+
+import { Groups } from '@screens/Groups'
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [loaded] =  useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+  
   return (
     <ThemeProvider theme={theme}>
       <StatusBar />
-      <Groups />
+      { loaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
