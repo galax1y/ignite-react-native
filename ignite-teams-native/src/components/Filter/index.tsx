4a3ba2544be14725980b031fddefc4e48cbf5b3a
LucasWermann <lucasvw.eng@gmail.com>
2023-06-08 16:08:57 -0300

    Add: Componente Filter

---
+++ ignite-teams-native/src/components/Filter/index.tsx
@@ -0,0 +1,19 @@
+import { TouchableOpacityProps } from 'react-native'
+import { Container, Title, FilterStyleProps } from './styles';
+
+type Props = TouchableOpacityProps & FilterStyleProps & {
+  title: string
+}
+
+export function Filter({ title, isActive=false, ...props} : Props) {
+  return (
+    <Container
+      isActive={isActive}
+      {...props}
+    >
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  )
+}
--- ignite-teams-native/src/screens/Players/index.tsx
+++ ignite-teams-native/src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
-import { Container, Form } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Container, Form, HeaderList, PlayerAmount } from './styles';
 
 import { Input } from '@components/Input';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Filter } from '@components/Filter';
 
 export function Players() {
+  const [team, setTeam] = useState<string>('')
+  const [players, setPlayers] = useState<string[]>([])
+
   return (
     <Container>
       <Header showBackButton />
@@ -23,6 +30,25 @@ export function Players() {
 
         <ButtonIcon icon='add' />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          horizontal
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+        />
+        
+        <PlayerAmount>
+          {players.length}
+        </PlayerAmount>
+      </HeaderList>
     </Container>
   )
 }
