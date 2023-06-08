b55acd3dbe8100df0f9a3631b2bd687566102dd8
LucasWermann <lucasvw.eng@gmail.com>
2023-06-07 22:04:09 -0300

    Add: Base do projeto 2, import alias & assets

---
+++ ignite-teams-native/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'react-native'
+import { Groups } from '@screens/Groups'
+
+export default function App() {
+  return (
+    <>
+      <StatusBar />
+      <Groups />
+    </>
+  );
+}
