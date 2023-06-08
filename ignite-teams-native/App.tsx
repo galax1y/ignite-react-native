2f940733336f7f51b414c2a6a649d8c2760acd18
LucasWermann <lucasvw.eng@gmail.com>
2023-06-07 22:52:42 -0300

    Add: Config do styled-components p/ React Native

--- ignite-teams-native/App.tsx
+++ ignite-teams-native/App.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from 'styled-components';
 import { StatusBar } from 'react-native'
 import { Groups } from '@screens/Groups'
+import theme from './src/theme'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar />
       <Groups />
-    </>
+    </ThemeProvider>
   );
 }
