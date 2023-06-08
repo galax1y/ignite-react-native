2e3231e72044cd14397a6bdb78bd3224c4cde369
LucasWermann <lucasvw.eng@gmail.com>
2023-06-08 12:58:31 -0300

    Add: Componente Button c/ 2 variantes

---
+++ ignite-teams-native/src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { ButtonTypeStyleProps, Container, Title } from './styles';
+
+type Props = TouchableOpacityProps & {
+  title: string
+  type?: ButtonTypeStyleProps
+}
+
+export function Button({ title, type='PRIMARY', ...props }: Props) {
+  return (
+      <Container
+        type={type}
+        {...props}
+      >
+        <Title>
+          {title}
+        </Title>
+      </Container>
+  )
+}
--- ignite-teams-native/src/screens/Groups/index.tsx
+++ ignite-teams-native/src/screens/Groups/index.tsx
@@ -7,6 +7,7 @@ import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -33,7 +34,9 @@ export function Groups() {
             message='Que tal cadastrar a primeira turma?'
           />
         )}
-      />      
+      />
+
+      <Button title='Criar nova turma' />   
     </Container>
   );
 }
